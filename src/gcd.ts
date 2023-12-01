389a670fa8a95e63caf2d70bfe8a661c4f2b1f45
kanaru-ssk <2019january.0131@gmail.com>
2023-12-02 03:44:04 +0900

    change conditions order

--- src/gcd.ts
+++ src/gcd.ts
@@ -1,7 +1,7 @@
 // 最大公約数
 
 export function gcd(a: number, b: number): number {
-  if (!Number.isInteger(a) || a < 0 || !Number.isInteger(b) || b < 0) return 0;
+  if (a < 0 || b < 0 || !Number.isInteger(a) || !Number.isInteger(b)) return 0;
 
   if (b === 0) return a;
   else return gcd(b, a % b);
