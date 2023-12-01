506651a97d3bf5a8c2b9cccef07a212830c9a11f
kanaru-ssk <2019january.0131@gmail.com>
2023-12-02 04:15:40 +0900

    adjust gcd

--- src/gcd.ts
+++ src/gcd.ts
@@ -3,6 +3,8 @@
 export function gcd(a: number, b: number): number {
   if (a < 0 || b < 0 || !Number.isInteger(a) || !Number.isInteger(b)) return 0;
 
-  if (b === 0) return a;
-  else return gcd(b, a % b);
+  // 割り切れるまで再帰的に呼び出す
+  if (b !== 0) return gcd(b, a % b);
+
+  return a;
 }
