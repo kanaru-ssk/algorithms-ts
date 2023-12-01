19c042e19cf800d146af324e9278a48efe49d143
kanaru-ssk <2019january.0131@gmail.com>
2023-12-02 02:56:58 +0900

    add error handling to factorial

--- src/factorial.ts
+++ src/factorial.ts
@@ -1,6 +1,9 @@
 // 階乗
 
 export function factorial(n: number): number {
+  // 階乗出来ない場合は0を返す
+  if (!Number.isInteger(n) || n < 0) return 0;
+
   let result = 1;
   for (let i = n; i > 1; i--) result *= i;
 
--- test/factorial.test.ts
+++ test/factorial.test.ts
@@ -19,3 +19,13 @@ test("args: 10 return: 3628800", () => {
   const result = factorial(10);
   expect(result).toBe(3628800);
 });
+
+test("args: 1.1 return: 0", () => {
+  const result = factorial(1.1);
+  expect(result).toBe(0);
+});
+
+test("args: -1 return: 0", () => {
+  const result = factorial(-1);
+  expect(result).toBe(0);
+});
