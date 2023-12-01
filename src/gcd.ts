85f5e329dd3cfd6b02133b1ee73cbba73e5174f4
kanaru-ssk <2019january.0131@gmail.com>
2023-12-02 03:34:15 +0900

    improve gcd

--- src/gcd.ts
+++ src/gcd.ts
@@ -1,12 +1,8 @@
+// 最大公約数
+
 export function gcd(a: number, b: number): number {
-  if (!Number.isInteger(a) || a <= 0 || !Number.isInteger(b) || b <= 0)
-    return 0;
+  if (!Number.isInteger(a) || a < 0 || !Number.isInteger(b) || b < 0) return 0;
 
-  while (b !== 0) {
-    const remain = a % b;
-    a = b;
-    b = remain;
-  }
-
-  return a;
+  if (b === 0) return a;
+  else return gcd(b, a % b);
 }
--- test/gcd.test.ts
+++ test/gcd.test.ts
@@ -5,9 +5,24 @@ test("args: (18, 12) return: 6", () => {
   expect(result).toBe(6);
 });
 
+test("args: (12, 18) return: 6", () => {
+  const result = gcd(12, 18);
+  expect(result).toBe(6);
+});
+
 test("args: (1, 1) return: 1", () => {
   const result = gcd(1, 1);
   expect(result).toBe(1);
+});
+
+test("args: (10, 0) return: 10", () => {
+  const result = gcd(10, 0);
+  expect(result).toBe(10);
+});
+
+test("args: (0, 10) return: 10", () => {
+  const result = gcd(0, 10);
+  expect(result).toBe(10);
 });
 
 test("args: (0, 0) return: 0", () => {
