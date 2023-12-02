51b0414a1b986ba3282c0f646f14ac023872b5a4
kanaru-ssk <2019january.0131@gmail.com>
2023-12-02 20:14:43 +0900

    add gcdArray

--- src/gcd.ts
+++ src/gcd.ts
@@ -1,10 +1,14 @@
 // 最大公約数
 
+// ユークリッド互除法
 export function gcd(a: number, b: number): number {
-  if (a < 0 || b < 0 || !Number.isInteger(a) || !Number.isInteger(b)) return 0;
-
-  if (b === 0) return a;
+  if (b === 0) return Math.abs(a);
 
   // 割り切れるまで再帰的に呼び出す
   return gcd(b, a % b);
+}
+
+export function gcdArray(array: number[]): number {
+  if (array.length === 0) return 0;
+  return array.reduce((result: number, num: number) => gcd(result, num));
 }
--- test/gcd.test.ts
+++ test/gcd.test.ts
@@ -1,41 +1,80 @@
-import { gcd } from "../src/gcd";
+import { gcd, gcdArray } from "../src/gcd";
 
-test("args: (18, 12) return: 6", () => {
-  const result = gcd(18, 12);
-  expect(result).toBe(6);
+describe("gcd", () => {
+  test("args: (18, 12) return: 6", () => {
+    const result = gcd(18, 12);
+    expect(result).toBe(6);
+  });
+
+  test("args: (12, 18) return: 6", () => {
+    const result = gcd(12, 18);
+    expect(result).toBe(6);
+  });
+
+  test("args: (1, 1) return: 1", () => {
+    const result = gcd(1, 1);
+    expect(result).toBe(1);
+  });
+
+  test("args: (10, 0) return: 10", () => {
+    const result = gcd(10, 0);
+    expect(result).toBe(10);
+  });
+
+  test("args: (0, 10) return: 10", () => {
+    const result = gcd(0, 10);
+    expect(result).toBe(10);
+  });
+
+  test("args: (0, 0) return: 0", () => {
+    const result = gcd(0, 0);
+    expect(result).toBe(0);
+  });
+
+  test("args: (10.5, 4.5) return: 1.5", () => {
+    const result = gcd(10.5, 4.5);
+    expect(result).toBe(1.5);
+  });
+
+  test("args: (-10, -15) return: 5", () => {
+    const result = gcd(-10, -15);
+    expect(result).toBe(5);
+  });
 });
 
-test("args: (12, 18) return: 6", () => {
-  const result = gcd(12, 18);
-  expect(result).toBe(6);
-});
+describe("gcdArray", () => {
+  test("args: [18, 12] return: 6", () => {
+    const result = gcdArray([18, 12]);
+    expect(result).toBe(6);
+  });
 
-test("args: (1, 1) return: 1", () => {
-  const result = gcd(1, 1);
-  expect(result).toBe(1);
-});
+  test("args: [36, 12, 48, 120] return: 12", () => {
+    const result = gcdArray([36, 12, 48, 120]);
+    expect(result).toBe(12);
+  });
 
-test("args: (10, 0) return: 10", () => {
-  const result = gcd(10, 0);
-  expect(result).toBe(10);
-});
+  test("args: [0, 0, 0] return: 0", () => {
+    const result = gcdArray([0, 0, 0]);
+    expect(result).toBe(0);
+  });
 
-test("args: (0, 10) return: 10", () => {
-  const result = gcd(0, 10);
-  expect(result).toBe(10);
-});
+  test("args: [-45, 0, 55] return: 5", () => {
+    const result = gcdArray([-45, 0, 55]);
+    expect(result).toBe(5);
+  });
 
-test("args: (0, 0) return: 0", () => {
-  const result = gcd(0, 0);
-  expect(result).toBe(0);
-});
+  test("args: [10.5, 0, 4.5] return: 1.5", () => {
+    const result = gcdArray([10.5, 0, 4.5]);
+    expect(result).toBe(1.5);
+  });
 
-test("args: (1.1, 1.2) return: 0", () => {
-  const result = gcd(1.1, 1.2);
-  expect(result).toBe(0);
-});
+  test("args: [3] return: 3", () => {
+    const result = gcdArray([3]);
+    expect(result).toBe(3);
+  });
 
-test("args: (-1, -2) return: 0", () => {
-  const result = gcd(-1, -2);
-  expect(result).toBe(0);
+  test("args: [] return: 0", () => {
+    const result = gcdArray([]);
+    expect(result).toBe(0);
+  });
 });
