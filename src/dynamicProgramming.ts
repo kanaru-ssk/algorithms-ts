de318dccf596f9d2cbe959d5389a786528a075ce
kanaru-ssk <2019january.0131@gmail.com>
2023-12-03 20:48:55 +0900

    change args of dynamicProgramming

--- src/dynamicProgramming.ts
+++ src/dynamicProgramming.ts
@@ -1,17 +1,19 @@
 // 動的計画法
 
-export function dynamicProgramming<T>(
-  tableH: number,
-  tableW: number,
-  calcCell: (table: T[][], y: number, x: number) => T
+export function dynamicProgramming<T, U>(
+  items: U[],
+  capacity: number,
+  calcCell: (y: number, x: number, table: T[][], items: U[]) => T
 ) {
+  const tableH = items.length + 1;
+  const tableW = capacity + 1;
   let table: T[][] = Array.from({ length: tableH }, () =>
     Array.from({ length: tableW })
   );
 
   for (let y = 0; y < tableH; y++) {
     for (let x = 0; x < tableW; x++) {
-      table[y][x] = calcCell(table, y, x);
+      table[y][x] = calcCell(y, x, table, items);
     }
   }
 
--- test/dynamicProgramming.test.ts
+++ test/dynamicProgramming.test.ts
@@ -1,7 +1,7 @@
 import { dynamicProgramming } from "../src/dynamicProgramming";
 
 describe("fibonacci number", () => {
-  function calcCell(table: number[][], y: number, x: number): number {
+  function calcCell(y: number, x: number, table: number[][]): number {
     if (x <= 0) return 0;
     if (x <= 2) return 1;
     return table[y][x - 1] + table[y][x - 2];
@@ -9,26 +9,31 @@ describe("fibonacci number", () => {
 
   test("n 1 return 1", () => {
     const n = 1;
-    const result = dynamicProgramming(1, n + 1, calcCell);
+    const result = dynamicProgramming([], n, calcCell);
     expect(result).toBe(1);
   });
 
   test("n 2 return 1", () => {
     const n = 2;
-    const result = dynamicProgramming(1, n + 1, calcCell);
+    const result = dynamicProgramming([], n, calcCell);
     expect(result).toBe(1);
   });
 
   test("n 6 return 8", () => {
     const n = 6;
-    const result = dynamicProgramming(1, n + 1, calcCell);
+    const result = dynamicProgramming([], n, calcCell);
     expect(result).toBe(8);
   });
 });
 
 describe("bin packing", () => {
   const items = [4, 7, 8, 5, 1];
-  function calcCell(table: boolean[][], y: number, x: number): boolean {
+  function calcCell(
+    y: number,
+    x: number,
+    table: boolean[][],
+    items: number[]
+  ): boolean {
     if (y === 0) return x === 0;
 
     // 上のマスがtrueの場合 : true
@@ -41,13 +46,13 @@ describe("bin packing", () => {
 
   test("pattern problem 1 return true", () => {
     const capacity = 10;
-    const result = dynamicProgramming(items.length + 1, capacity + 1, calcCell);
+    const result = dynamicProgramming(items, capacity, calcCell);
     expect(result).toBe(true);
   });
 
   test("pattern problem 1 return true", () => {
     const capacity = 22;
-    const result = dynamicProgramming(items.length + 1, capacity + 1, calcCell);
+    const result = dynamicProgramming(items, capacity, calcCell);
     expect(result).toBe(false);
   });
 });
@@ -64,28 +69,33 @@ describe("knapsack", () => {
     { cost: 4, value: 5 },
     { cost: 1, value: 1 },
   ];
-  function calcCell(table: number[][], y: number, x: number): number {
+  function calcCell(
+    y: number,
+    x: number,
+    table: number[][],
+    items: Item[]
+  ): number {
     if (y === 0) return 0;
     // items[i]が入らない場合 : 上のマスを代入
     if (x < items[y - 1].cost) return table[y - 1][x];
     // items[i]が入る場合 : 上のマスと比較して大きい方を代入
-    else
-      return Math.max(
-        table[y - 1][x - items[y - 1].cost] + items[y - 1].value,
-        table[y - 1][x]
-      );
+
+    return Math.max(
+      table[y - 1][x - items[y - 1].cost] + items[y - 1].value,
+      table[y - 1][x]
+    );
   }
 
   test("pattern 1 return 16", () => {
     const capacity = 10;
-    const result = dynamicProgramming(items.length + 1, capacity + 1, calcCell);
+    const result = dynamicProgramming(items, capacity, calcCell);
 
     expect(result).toBe(16);
   });
 
   test("pattern 2 return 25", () => {
     const capacity = 30;
-    const result = dynamicProgramming(items.length + 1, capacity + 1, calcCell);
+    const result = dynamicProgramming(items, capacity, calcCell);
 
     expect(result).toBe(25);
   });
