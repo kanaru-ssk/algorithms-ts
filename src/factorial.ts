3249eadb17e6717bca95b126d707c03c0a60e35a
kanaru-ssk <2019january.0131@gmail.com>
2023-12-04 15:45:16 +0900

    Refactor factorial functions

--- src/factorial.ts
+++ src/factorial.ts
@@ -4,8 +4,7 @@ export function factorial(n: number): number {
   // 階乗出来ない場合は0を返す
   if (!Number.isInteger(n) || n < 0) return 0;
 
-  let result = 1;
-  for (let i = n; i > 1; --i) result *= i;
+  if (n <= 1) return 1;
 
-  return result;
+  return n * factorial(n - 1);
 }
