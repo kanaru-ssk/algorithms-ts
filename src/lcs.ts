05a56d0389f65617d589f9bd13c8ccd8533ef125
kanaru-ssk <2019january.0131@gmail.com>
2023-12-04 23:25:34 +0900

    change lcs to not use dynamicProgramming

--- src/lcs.ts
+++ src/lcs.ts
@@ -1,19 +1,21 @@
 // 最長共通部分列 (Longest Common Subsequence)
 
-import { dynamicProgramming } from "./dynamicProgramming";
-
 export function lcs(a: string, b: string): number {
-  function calcCell(
-    table: number[],
-    [y, x]: number[],
-    element: { a: string; b: string }
-  ): number {
-    if (y === 0 || x === 0) return 0;
-    const h = element.b.length + 1;
-    if (element.a.charAt(y - 1) === element.b.charAt(x - 1))
-      return table[(y - 1) * h + x - 1] + 1;
-    return Math.max(table[(y - 1) * h + x], table[y * h + x - 1]);
+  const tableH = a.length + 1;
+  const tableW = b.length + 1;
+  const table: number[][] = Array.from({ length: tableH }, () =>
+    Array(tableW).fill(0)
+  );
+
+  for (let y = 1; y < tableH; y++) {
+    for (let x = 1; x < tableW; x++) {
+      if (a[y - 1] === b[x - 1]) {
+        table[y][x] = table[y - 1][x - 1] + 1;
+      } else {
+        table[y][x] = Math.max(table[y - 1][x], table[y][x - 1]);
+      }
+    }
   }
 
-  return dynamicProgramming({ a, b }, calcCell);
+  return table[tableH - 1][tableW - 1];
 }
